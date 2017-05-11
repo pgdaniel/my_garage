1ee1796beb942dc2bd67d1c9fa5b7e5d000e9046
Paul Daniel <paulgdan@gmail.com>
2017-05-11 12:01:33 -0700

    fix

--- app/uploaders/home_image_uploader.rb
+++ app/uploaders/home_image_uploader.rb
@@ -9,6 +9,7 @@ class HomeImageUploader < CarrierWave::Uploader::Base
     storage :file
   else
     storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
