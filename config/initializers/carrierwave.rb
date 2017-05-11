d2f9c7f1ffd2c6e161e8601ebd73710971137207
Paul Daniel <paulgdan@gmail.com>
2017-05-11 12:15:03 -0700

    fix bucket name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ if Rails.env.production?
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region:                'us-west-2',
     }
-    config.fog_directory  = 'big-ol-garage'
+    config.fog_directory  = 'my-big-ol-garage'
     config.fog_public     = false
   end
 end
